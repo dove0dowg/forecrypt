6f280c87df63be5f54e47cf9612a8ac14a14e4c0
dove dowg <dove000dowg@gmail.com>
2025-01-07 18:03:26 +0700

    module for quick table deletion in the database for development purposes

---
+++ utils/db_reset.py
@@ -0,0 +1,22 @@
+import psycopg2
+from psycopg2.extras import execute_values
+import logging
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import db_utils
+
+# configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    conn = db_utils.init_database_connection()
+    db_utils.delete_view_and_tables(conn)
