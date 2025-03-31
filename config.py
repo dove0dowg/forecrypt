47cd6f211eae3a69f1cd2f3cc2692e4c7992e44e
dove dowg <dove000dowg@gmail.com>
2025-03-31 20:11:35 +0700

    Clickhouse config dictionary addition and following naming changes for Postgres variables

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
 #  Project config
 CRYPTO_LIST = ['BTC', 'ETH'] #, 'DOGE', 'ADA', 'SOL']
-START_DATE = "2024-10-01T00:00:00" # first day and hour of historical data in naive.
-FINISH_DATE = "2025-01-01T00:00:00" # last day and hour of historical data in naive, "yyyy-mm-ddThh:mm:ss" format.
+START_DATE = "2025-03-01T00:00:00" # first day and hour of historical data in naive.
+FINISH_DATE = "2025-03-05T00:00:00" # last day and hour of historical data in naive, "yyyy-mm-ddThh:mm:ss" format.
 # ---------------------------------------------------------
 # Directory for check, save and load models. Mostly used in models_processing.py
 MODELS_DIRECTORY = r"C:\forecrypt_models"
 # ---------------------------------------------------------
-#   Database configuration.
+#   Postgres database configuration.
 #    It is empty, as ForecrypT using environmental variables with higher priority:
-#    FORECRYPT_DB_NAME=forecrypt_db
-#    FORECRYPT_DB_USER=SET_YOUR_USER
-#    FORECRYPT_DB_PASS=SET_YOUR_PASS
-#    FORECRYPT_DB_HOST=localhost
-#    FORECRYPT_DB_PORT=5432
-#    FORECRYPT_API_KEY=YOUR_API_KEY
+#    FORECRYPT_PG_DB_NAME=forecrypt_db
+#    FORECRYPT_PG_DB_USER=SET_YOUR_USER
+#    FORECRYPT_PG_DB_PASS=SET_YOUR_PASS
+#    FORECRYPT_PG_DB_HOST=localhost
+#    FORECRYPT_PG_DB_PORT=5432
 #    You can create your .env file in project's directory. Check README.md.
 #    Or just enter your database config below, if security doesn't matter much:
 # ---------------------------------------------------------
@@ -23,6 +22,31 @@ PG_DB_CONFIG = {
     'password': '',
     'host': 'localhost',
     'port': 5432
+}
+# ---------------------------------------------------------
+#   Clickhouse database configuration.
+#    It is empty, as ForecrypT using environmental variables with higher priority:
+#    FORECRYPT_CH_DB_NAME=
+#    FORECRYPT_CH_DB_USER=SET_YOUR_USER
+#    FORECRYPT_CH_DB_PASS=SET_YOUR_PASS
+#    FORECRYPT_CH_DB_HOST=localhost
+#    FORECRYPT_CH_DB_PORT=
+#    You can create your .env file in project's directory. Check README.md.
+#    Or just enter your database config below, if security doesn't matter much:
+# ---------------------------------------------------------
+CH_DB_CONFIG = {
+    'database': '', 
+    'user': '',
+    'password': '',
+    'host': 'localhost',
+    'port': 9000,
+    'http_port': 8123,
+    'interserver_port': 9009,
+    'container_name': 'clickhouse_container',
+    'default_user_xml_path': '/etc/clickhouse-server/users.d/default-user.xml',
+    'users_xml_path': '/etc/clickhouse-server/users.xml',
+    'db_data_wsl_dir': '/var/lib/clickhouse/data',  #
+    'config_wsl_dir': '/var/lib/clickhouse/config',  #
 }
 # ---------------------------------------------------------
 #   Model parameters. Two letters like [TD] means technical abbreviation mostly used for model objects and forecast naming in database. 
