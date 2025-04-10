6b7b6e86e1996c70c2c56494a86ebe580c211fe9
dove dowg <dove000dowg@gmail.com>
2025-04-10 22:50:56 +0700

    Module now uses gen_sql_* functions from core_columns_generators.py to create SQL-strings for queries.
    Changes made to control all SQL-pipeline in one module instead of four.

--- db/pg_to_ch_pipeline.py
+++ db/pg_to_ch_pipeline.py
@@ -4,6 +4,12 @@ import subprocess
 import json
 from typing import Any
 from clickhouse_driver import Client
+# modules
+from .core_columns_generators import (
+    gen_sql_ch_create_external_pg_table,
+    gen_sql_ch_create_forecast_data_table,
+    gen_sql_ch_insert_from_external
+)
 # logger
 logger = logging.getLogger(__name__)
 
@@ -50,31 +56,20 @@ def create_external_pg_table(clickhouse_client: Client, clickhouse_config: dict[
     ch_database_name = clickhouse_config['database']
     ch_table_name = clickhouse_config['table']
     
-    # Prepare the query to create the table in ClickHouse
-    query = f"""
-    CREATE TABLE {ch_database_name}.{ch_table_name}
-    (
-        id String,
-        timestamp DateTime,
-        currency String,
-        forecast_step Int32,
-        forecast_value Float64,
-        historical_value Float64,
-        model String,
-        model_name_ext String,
-        external_model_params String,
-        inner_model_params String,
-        h_uploaded_at DateTime,
-        f_uploaded_at DateTime,
-        config_start DateTime,
-        config_end DateTime
-    ) 
-    ENGINE = PostgreSQL('{pg_container_ip}:5432', '{pg_database}', 'backtest_data_mv', '{pg_user}', '{pg_password}');
-    """
-    logger.debug(query)
+    # Prepare the query to create external Postgres Table in ClickHouse
+    sql_ch_create_external_pg_table = gen_sql_ch_create_external_pg_table(
+        pg_container_ip,  
+        pg_database, 
+        pg_user, 
+        pg_password, 
+        ch_database_name, 
+        ch_table_name
+        )
+
+    logger.debug(sql_ch_create_external_pg_table)
     # Execute the query via the provided ClickHouse client
     try:
-        clickhouse_client.execute(query)  # Use the provided client to execute the query
+        clickhouse_client.execute(sql_ch_create_external_pg_table)  # Use the provided client to execute the query
         logger.info(f"Table {ch_table_name} created successfully.")
         return True  # Success
     except Exception as e:
@@ -99,28 +94,11 @@ def create_ch_forecast_data_table(clickhouse_client: Client, clickhouse_config: 
     ch_database = clickhouse_config['database']
     local_table_name = "forecast_data"
 
-    query = f"""
-    CREATE TABLE IF NOT EXISTS {ch_database}.{local_table_name} (
-        id String,
-        timestamp DateTime,
-        currency String,
-        forecast_step Int32,
-        forecast_value Float64,
-        historical_value Float64,
-        model String,
-        model_name_ext String,
-        external_model_params String,
-        inner_model_params String,
-        h_uploaded_at DateTime,
-        f_uploaded_at DateTime
-    )
-    ENGINE = ReplacingMergeTree(h_uploaded_at)
-    PARTITION BY toYYYYMM(timestamp)
-    ORDER BY (timestamp, currency, model, forecast_step);
-    """
+    # Prepare the query to create inner data table in ClickHouse
+    sql_ch_create_forecast_data_table = gen_sql_ch_create_forecast_data_table(ch_database)
 
     try:
-        clickhouse_client.execute(query)
+        clickhouse_client.execute(sql_ch_create_forecast_data_table)
         logger.info(f"Local ClickHouse table '{local_table_name}' created successfully.")
         return True
     except Exception as e:
@@ -130,26 +108,10 @@ def create_ch_forecast_data_table(clickhouse_client: Client, clickhouse_config: 
 def insert_from_external(clickhouse_client: Client, clickhouse_config: dict[str, Any]) -> bool:
     ch_database = clickhouse_config['database']
 
-    query = f"""
-    INSERT INTO {ch_database}.forecast_data
-    SELECT
-        id,
-        timestamp,
-        currency,
-        forecast_step,
-        forecast_value,
-        historical_value,
-        model,
-        model_name_ext,
-        external_model_params,
-        inner_model_params,
-        h_uploaded_at, 
-        f_uploaded_at
-    FROM {ch_database}.external_pg_forecast_data
-    """
+    sql_ch_insert_from_external = gen_sql_ch_insert_from_external(ch_database)
 
     try:
-        clickhouse_client.execute(query)
+        clickhouse_client.execute(sql_ch_insert_from_external)
         logger.info("Bulk inserted data from external_pg_forecast_data into forecast_data.")
         return True
     except Exception as e:
